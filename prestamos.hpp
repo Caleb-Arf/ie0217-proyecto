54329e61d5fcf503325470796839bb72ec268750
eve2030f <143893444+eve2030f@users.noreply.github.com>
2024-06-05 23:17:34 -0600

    agregando destructor

--- prestamos.hpp
+++ prestamos.hpp
@@ -25,17 +25,12 @@ private:
 public:
     // Constructor
     Prestamo(int id, double monto, double interes, int meses, int anios, string frecuencia);
-
-    // Method to solicit a loan
+    //Destructor
+    ~Prestamo();
+//Metodos
     void solicitarPrestamo(double monto, double interes, int meses, int anios, string frecuencia);
-
-    // Method to check loan status
     void estadoPrestamo();
-
-    // Method to make a payment
     void abonoPrestamo(double monto);
-
-    // Method to make an extra payment
     void abonoExtraordinario(double monto);
 };
 
